d33fa44400ff9e6fa3698a1a6b766338c21ca190
Mimoh6 <mimohbagde3@gmail.com>
2025-10-19 17:05:46 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,31 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
-  key_name = "LinuxUbuntu"
+  ami                    = "ami-0360c520857e3138f"
+  instance_type          = "t2.micro"
+  key_name               = "LinuxUbuntu"
   vpc_security_group_ids = ["sg-0cd9e5f943ccf5c5e"]
+
   connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./LinuxUbuntu.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./LinuxUbuntu.pem")
+    host        = self.public_ip
+  }
+
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = [
+      "echo 'wait to start the instance'"
+    ]
   }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/Finance-Project/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server"
   }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Finance-Project/terraform-files/ansibleplaybook.yml"
+  }
+}
